879bcb5e5bcfb8bf8e46ba0a7b6727df2e516bf8
tuana <https://github.com/TuanAnhmvp/Android-Studio>
2022-09-12 14:00:48 +0700

    start

---
+++ app/src/main/java/com/example/dagger_hilt_mvvm_room_ld_nav_coroutines/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dagger_hilt_mvvm_room_ld_nav_coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

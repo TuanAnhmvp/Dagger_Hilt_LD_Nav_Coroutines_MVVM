e9f92810819b2f066533ab58cd232f19ac6e2da0
tuana <https://github.com/TuanAnhmvp/Android-Studio>
2022-09-13 22:15:47 +0700

    nav

--- app/src/main/java/com/example/dagger_hilt_mvvm_room_ld_nav_coroutines/MainActivity.kt
+++ app/src/main/java/com/example/dagger_hilt_mvvm_room_ld_nav_coroutines/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.dagger_hilt_mvvm_room_ld_nav_coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.dagger_hilt_mvvm_room_ld_nav_coroutines.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //View binding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mNavController: NavController
+    private lateinit var mAppBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFrag =
+            supportFragmentManager.findFragmentById(R.id.fragmentHost) as NavHostFragment
+
+        mNavController = navHostFrag.navController
+
+        setupActionBarWithNavController(mNavController)
+        mAppBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.todoListFragment,
+                R.id.addTodoFragment,
+            )
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return mNavController.navigateUp(mAppBarConfiguration) || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/dagger_hilt_mvvm_room_ld_nav_coroutines/ui/TodoListFragment.kt
+++ app/src/main/java/com/example/dagger_hilt_mvvm_room_ld_nav_coroutines/ui/TodoListFragment.kt
@@ -1,5 +1,4 @@
 package com.example.dagger_hilt_mvvm_room_ld_nav_coroutines.ui
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,31 +6,34 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import com.example.dagger_hilt_mvvm_room_ld_nav_coroutines.R
-import com.example.dagger_hilt_mvvm_room_ld_nav_coroutines.databinding.FragmentAddTodoBinding
+import com.example.dagger_hilt_mvvm_room_ld_nav_coroutines.databinding.FragmentTodoListBinding
 
 
-class AddTodoFragment : Fragment() {
+class TodoListFragment : Fragment() {
 
-    private var _binding: FragmentAddTodoBinding? = null
+    private var _binding: FragmentTodoListBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        _binding = FragmentAddTodoBinding.inflate(inflater, container, false)
+        _binding = FragmentTodoListBinding.inflate(
+            inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnCancel.setOnClickListener {
+
+        binding.fabAddTask.setOnClickListener {
             view.findNavController().navigate(
-                R.id.action_addTodoFragment_to_todoListFragment
+                R.id.action_todoListFragment_to_addTodoFragment
             )
         }
+
+
+
     }
 
 
